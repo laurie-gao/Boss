674a371ddc1ff42b909287ec9046899513bfb496
Laurie Gao <lauriegao123@gmail.com>
2020-02-01 00:06:44 -0500

    weekdays functional

--- src/components/TaskDate.js
+++ src/components/TaskDate.js
@@ -2,9 +2,27 @@
 import React from 'react';
 import moment from 'moment';
 import { FaRegCalendarCheck, FaRegHandPointRight, FaRegPaperPlane } from 'react-icons/fa';
+import { getDayOfWeek } from '../helpers';
 
-export const TaskDate = ({ setTaskDate, showTaskDate, setShowTaskDate }) =>
-  showTaskDate && (
+export const TaskDate = ({ setTaskDate, showTaskDate, setShowTaskDate }) => {
+  const todayKey = getDayOfWeek(moment().weekday()).key;
+
+  const setDate = (day) => { return (
+      <div
+        onClick={() => {
+          setShowTaskDate(false);
+          setTaskDate(moment().add(day, 'days').format('DD/MM/YYYY'));
+        }}
+        data-test-id="task-date-next-week"
+      >
+        <span>
+          <FaRegPaperPlane />
+        </span>
+        <span>{getDayOfWeek((todayKey+day)%7).name}</span>
+      </div>
+  )};
+
+  return (showTaskDate && (
     <div className="task-date" data-testid="task-date-overlay">
       <ul className="task-date__list">
         <li>
@@ -25,11 +43,7 @@ export const TaskDate = ({ setTaskDate, showTaskDate, setShowTaskDate }) =>
           <div
             onClick={() => {
               setShowTaskDate(false);
-              setTaskDate(
-                moment()
-                  .add(1, 'day')
-                  .format('DD/MM/YYYY')
-              );
+              setTaskDate(moment().add(1, 'day').format('DD/MM/YYYY'));
             }}
             data-testid="task-date-tomorrow"
           >
@@ -39,24 +53,11 @@ export const TaskDate = ({ setTaskDate, showTaskDate, setShowTaskDate }) =>
             <span>Tomorrow</span>
           </div>
         </li>
-        <li>
-          <div
-            onClick={() => {
-              setShowTaskDate(false);
-              setTaskDate(
-                moment()
-                  .add(7, 'days')
-                  .format('DD/MM/YYYY')
-              );
-            }}
-            data-testid="task-date-next-week"
-          >
-            <span>
-              <FaRegPaperPlane />
-            </span>
-            <span>Next week</span>
-          </div>
-        </li>
+        <li>{setDate((todayKey+2)%7)}</li>
+        <li>{setDate((todayKey+3)%7)}</li>
+        <li>{setDate((todayKey+4)%7)}</li>
+        <li>{setDate((todayKey+5)%7)}</li>
+        <li>{setDate((todayKey+6)%7)}</li>
       </ul>
     </div>
-  );
+  ))};
